84867728ae88a1cc5d636662bba3bca4dcdb38b8
Jatin2102786 <jatinmehmi790@gmail.com>
2023-07-08 12:29:27 +0530

    commit

--- app/src/main/java/com/jatin/kotlin_task3/MainActivity.kt
+++ app/src/main/java/com/jatin/kotlin_task3/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.jatin.kotlin_task3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        lateinit var snackBar:Button
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        snackBar= findViewById(R.id.btn_snack)
+        snackBar.setOnClickListener {
+            var snackbar = Snackbar.make(it,"Snackbar",Snackbar.LENGTH_SHORT).setAction("intent",
+                View.OnClickListener {
+                    var intent = Intent(this,MainActivity2::class.java)
+                    startActivity(intent)
+                })
+                .show()
+
+        }
     }
 }
